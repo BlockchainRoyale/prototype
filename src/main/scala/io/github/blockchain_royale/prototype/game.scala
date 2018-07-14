d88528f1d732fc559c0b78a99576dbd52693a1e4
Pablo Duboue <pablo.duboue@gmail.com>
2018-07-14 12:46:54 -0400

    only combat remains now

--- src/main/scala/io/github/blockchain_royale/prototype/game.scala
+++ src/main/scala/io/github/blockchain_royale/prototype/game.scala
@@ -2,7 +2,7 @@ package io.github.blockchain_royale.prototype
 
 import scala.util.Random
 
-abstract class ObjectType {
+sealed abstract class ObjectType {
   val name: String
 }
 
@@ -23,27 +23,94 @@ object Types {
   type ObjectId = Int
   type GameId = Int
   type Coord = Tuple2[Int, Int]
+}
 
+object Consts {
+  val ALL_OBJECTS = List(Gun, Grenade, Shield).toArray
+  val NUM_PLAYERS = 4
+  val MAP_SIZE = 5
+  val TOTAL_OBJECTS = 9
+  val ARENA_SIZE = 5
 }
 
 import Types._
+import Consts._
+import org.json.JSONArray
+import org.json.JSONML
+import org.json.JSONTokener
+import org.json.JSONObject
+import org.apache.commons.codec.digest.DigestUtils
 
 case class Player(id: PlayerId, name: String)
-
 case class Object(id: ObjectId, _type: ObjectType)
-
 case class Room(location: Coord, objects: List[ObjectId], players: List[PlayerId], open: Boolean)
-
 case class Stats(alive: Boolean, kills: List[PlayerId])
-
 case class GameMap(rooms: Array[Room])
+
+// player actions, for combat
+
+sealed abstract class PlayerAction
+case class MovePA(dir: Tuple2[Int, Int]) extends PlayerAction
+case class ShootPA(dir: Tuple2[Int, Int], obj: ObjectId) extends PlayerAction
+case class TurnTowardsPlayerPA() extends PlayerAction
+case class MoveForwardPA() extends PlayerAction
+case class MoveBackwardPA() extends PlayerAction
+case class TurnLeftPA() extends PlayerAction
+case class TurnRightPA() extends PlayerAction
+case class UsePA(obj: ObjectId) extends PlayerAction
+
+case class Outcome(alive: List[PlayerId], dead: List[PlayerId])
+
+// game actions, for the chain
+sealed abstract class GameAction {
+  val timestamp = System.currentTimeMillis()
+  val player: PlayerId
+}
+case class GenesisAct() extends GameAction { val player = -1 }
+case class NewPlayerAct(player: PlayerId, name: String) extends GameAction
+case class PlayerMoveAct(player: PlayerId, dir: String) extends GameAction
+case class PlayerPickAct(player: PlayerId, obj: ObjectId) extends GameAction
+case class PlayerStartAttackAct(player: PlayerId, other: PlayerId, actionsHash: String) extends GameAction
+case class PlayerFinishAttackAct(player: PlayerId, other: PlayerId, actionsString: String) extends GameAction {
+  private var cache: List[PlayerAction] = null
+  def actions: List[PlayerAction] = {
+    if (cache == null) {
+      val parser = new JSONTokener(actionsString)
+      val top = new JSONArray(parser)
+      cache = (for (idx <- 0.to(top.length() - 1)) yield {
+        val obj = top.getJSONObject(idx)
+        obj.getString("type") match {
+          case "MoveNorth"         => MovePA((-1, 0))
+          case "MoveSouth"         => MovePA((1, 0))
+          case "MoveWest"          => MovePA((0, -1))
+          case "MoveEast"          => MovePA((0, 1))
+          case "ShootNorth"        => ShootPA((-1, 0), obj.getInt("object"))
+          case "ShootSouth"        => ShootPA((1, 0), obj.getInt("object"))
+          case "ShootWest"         => ShootPA((0, -1), obj.getInt("object"))
+          case "ShootEast"         => ShootPA((0, 1), obj.getInt("object"))
+          case "TurnTowardsPlayer" => TurnTowardsPlayerPA()
+          case "MoveForward"       => MoveForwardPA()
+          case "MoveBackward"      => MoveBackwardPA()
+          case "TurnLeft"          => TurnLeftPA()
+          case "TurnRight"         => TurnRightPA()
+          case "UsePA"             => UsePA(obj.getInt("object"))
+        }
+      }).toList
+    }
+    cache
+  }
+}
+case class CombatAct(a: PlayerId, b: PlayerId, startA: Coord, startB: Coord, actionsA: String, actionsB: String, outcome: Outcome) extends GameAction {
+  val player = -1
+}
 
 case class Game(id: GameId,
                 players: Map[PlayerId, Player] = Map(),
                 objects: Map[ObjectId, Object] = Map(),
                 holding: Map[PlayerId, List[ObjectId]] = Map(),
                 stats: Map[PlayerId, Stats] = Map(),
-                map: GameMap) {
+                map: GameMap,
+                chain: List[GameAction]) {
   def start: Game = {
 
     var objectCount = 0
@@ -52,13 +119,7 @@ case class Game(id: GameId,
     var newHolding: Map[PlayerId, List[ObjectId]] = Map()
 
     // given guns
-    val newObject = (objectId: ObjectId) => {
-      Random.nextInt(3) match {
-        case 0 => Object(objectId, Gun)
-        case 1 => Object(objectId, Grenade)
-        case 2 => Object(objectId, Shield)
-      }
-    }
+    def newObject(objectId: ObjectId) = Object(objectId, ALL_OBJECTS(Random.nextInt(ALL_OBJECTS.size)))
     for (_id <- players.keys) {
       val objectId = objectCount
       objectCount += 1
@@ -70,8 +131,8 @@ case class Game(id: GameId,
 
     // drop objects
     var objectsAtRoom: Map[Coord, List[ObjectId]] = Map()
-    for (_ <- 1.to(10)) {
-      val coord = Tuple2(Random.nextInt(5), Random.nextInt(5))
+    for (_ <- 1.to(TOTAL_OBJECTS - NUM_PLAYERS)) {
+      val coord = Tuple2(Random.nextInt(MAP_SIZE), Random.nextInt(MAP_SIZE))
       val objectId = objectCount
       objectCount += 1
       objectsAtRoom += (coord -> (objectsAtRoom.getOrElse(coord, List()) ++ List(objectId)))
@@ -81,14 +142,14 @@ case class Game(id: GameId,
     // pick initial position for players
     var initialPos: Map[Coord, List[PlayerId]] = Map()
     for (_id <- players.keys) {
-      val coord = Tuple2(Random.nextInt(5), Random.nextInt(5))
+      val coord = Tuple2(Random.nextInt(MAP_SIZE), Random.nextInt(5))
       initialPos += (coord -> (initialPos.getOrElse(coord, List()) ++ List(_id)))
     }
 
     // build map
     var rooms: Map[Coord, Room] = Map()
-    for (x <- 0.to(4)) {
-      for (y <- 0.to(4)) {
+    for (x <- 0.to(MAP_SIZE - 1)) {
+      for (y <- 0.to(MAP_SIZE - 1)) {
         val coord = Tuple2(x, y)
         val room = Room(coord, objectsAtRoom.getOrElse(coord, List()), initialPos.getOrElse(coord, List()), true)
         rooms += coord -> room
@@ -102,4 +163,78 @@ case class Game(id: GameId,
   }
 }
 
+object GameUtils {
+  def toJson(game: Game) = {
+    val obj = new JSONObject()
+    var players: Map[PlayerId, JSONObject] = Map()
+    for (player <- game.players.values) {
+      val playerObj = new JSONObject()
+      val stats = game.stats(player.id)
+      playerObj.put("id", player.id)
+      playerObj.put("name", player.name)
+      playerObj.put("alive", stats.alive)
+      val killsArray = new JSONArray()
+      for (killed <- stats.kills) {
+        killsArray.put(killed)
+      }
+      playerObj.put("kills", killsArray)
+      val holdArray = new JSONArray()
+      for (holdId <- game.holding(player.id)) {
+        holdArray.put(game.objects(holdId)._type.name)
+      }
+      playerObj.put("holds", holdArray)
 
+      players += player.id -> playerObj
+    }
+
+    val rows = new JSONArray()
+    for (r <- 0.to(4)) {
+      val row = new JSONArray()
+      for (c <- 0.to(4)) {
+        val roomObj = new JSONObject()
+        roomObj.put("row", r)
+        roomObj.put("col", c)
+        val playersInRoom = new JSONArray()
+        val room = game.map.rooms.find(_.location == Tuple2(r, c)).get
+        for (player <- room.players) {
+          players(player).put("row", r)
+          players(player).put("col", c)
+          playersInRoom.put(player)
+        }
+        val objectsInRoom = new JSONArray()
+        for (obj <- room.objects) {
+          val objObj = new JSONObject()
+          objObj.put("id", obj)
+          objObj.put("type", game.objects(obj)._type.name)
+          objectsInRoom.put(objObj)
+        }
+        roomObj.put("players", playersInRoom)
+        roomObj.put("objects", objectsInRoom)
+        roomObj.put("active", room.open)
+        row.put(roomObj)
+      }
+      rows.put(row)
+    }
+
+    val playerArray = new JSONArray()
+    for (playerObj <- players.values) {
+      playerArray.put(playerObj)
+    }
+
+    val actionArray = new JSONArray()
+    for (act <- game.chain) {
+      actionArray.put(act.toString)
+    }
+    obj.put("players", playerArray)
+    obj.put("rooms", rows)
+    obj.put("actions", actionArray)
+
+    obj.toString
+  }
+
+  def hash(str: String): String = DigestUtils.sha1Hex(str)
+
+  def combat(a: PlayerFinishAttackAct, b: PlayerFinishAttackAct): Tuple3[Coord, Coord, Outcome] = {
+    ((0, 0), (1, 1), Outcome(List(a.player, b.player), List()))
+  }
+}
--- src/main/scala/io/github/blockchain_royale/prototype/servlet.scala
+++ src/main/scala/io/github/blockchain_royale/prototype/servlet.scala
@@ -9,6 +9,7 @@ class GameServlet extends ScalatraServlet {
   private var games: Map[Int, Game] = Map[Int, Game]()
 
   import Types._
+  import Consts._
 
   get("/") {
 
@@ -17,7 +18,7 @@ class GameServlet extends ScalatraServlet {
   post("/api/new_game") {
     games.synchronized {
       val newId = games.size + 1
-      games = games + (newId -> Game(newId, map = GameMap(Array())))
+      games = games + (newId -> Game(newId, map = GameMap(Array()), chain = List(GenesisAct())))
       newId.toString
     }
   }
@@ -27,15 +28,16 @@ class GameServlet extends ScalatraServlet {
       val game = games(params("game").toInt)
       val newPlayer = Player(game.players.size + 1, params("name"))
       val newGame = game.copy(players = game.players + (newPlayer.id -> newPlayer))
+      //TODO read public key from stdin
 
-      val potentiallyStartedGame =
-        if (newGame.players.size == 6)
-          newGame.start
+      val (potentiallyStartedGame, started) =
+        if (newGame.players.size == NUM_PLAYERS)
+          (newGame.start, true)
         else
-          newGame
+          (newGame, false)
 
-      games = games + (game.id -> potentiallyStartedGame)
-      newPlayer.id.toString
+      games = games + (game.id -> potentiallyStartedGame.copy(chain = NewPlayerAct(newPlayer.id, newPlayer.name) :: potentiallyStartedGame.chain))
+      s"${newPlayer.id.toString} $started"
     }
   }
 
@@ -57,12 +59,12 @@ class GameServlet extends ScalatraServlet {
         game.map.rooms(roomIdx) = room.copy(players = room.players.filterNot(_ == playerId))
         game.map.rooms(destRoomIdx) = destRoom.copy(players = destRoom.players ++ List(playerId))
 
-        if (!destRoom.open) {
-          games += gameId -> game.copy(stats = game.stats + (playerId -> (game.stats(playerId).copy(alive = false))))
-        }
+        val potentiallyKilledPlayer = if (!destRoom.open) game.copy(stats = game.stats + (playerId -> (game.stats(playerId).copy(alive = false)))) else game
+
+        games += gameId -> potentiallyKilledPlayer.copy(chain = PlayerMoveAct(playerId, params("direction")) :: game.chain)
         newCoord
       } else {
-        "Dead players tell no lies"
+        "400 Dead players tell no lies"
       }
     }
   }
@@ -78,13 +80,102 @@ class GameServlet extends ScalatraServlet {
         val (room, roomIdx) = game.map.rooms.zipWithIndex.find(_._1.players.contains(playerId)).get
         if (room.objects.contains(objectId)) {
           game.map.rooms(roomIdx) = room.copy(objects = room.objects.filterNot(_ == objectId))
-          games += gameId -> game.copy(holding = game.holding + (playerId -> (game.holding(playerId) ++ List (objectId))))
+          games += gameId -> game.copy(holding = game.holding + (playerId -> (game.holding(playerId) ++ List(objectId))),
+            chain = PlayerPickAct(playerId, objectId) :: game.chain)
           objectId
         } else {
-          "object is not there"
+          "400 object is not there"
         }
       } else {
-        "Dead players tell no lies"
+        "400 Dead players tell no lies"
+      }
+    }
+  }
+
+  post("/api/attack/:game/:player/start/:other") {
+    games.synchronized {
+      val gameId = params("game").toInt
+      val game = games(gameId)
+      val playerId = params("player").toInt
+      val otherId = params("other").toInt
+      val actionsHash = request.body
+      val statsOwn = game.stats(playerId)
+      val statsOther = game.stats(playerId)
+      if (statsOwn.alive) {
+        if (statsOther.alive) {
+          val (roomOwn, roomOwnIdx) = game.map.rooms.zipWithIndex.find(_._1.players.contains(playerId)).get
+          val (roomOther, roomOtherIdx) = game.map.rooms.zipWithIndex.find(_._1.players.contains(otherId)).get
+          if (roomOwnIdx != roomOtherIdx) {
+            games += gameId -> game.copy(chain = PlayerStartAttackAct(playerId, otherId, actionsHash) :: game.chain)
+            actionsHash
+          } else {
+            "400 player is not there"
+          }
+        } else {
+          "400 It's dead Jim, it's dead"
+        }
+      } else {
+        "400 Dead players tell no lies"
+      }
+    }
+  }
+
+  post("/api/attack/:game/:player/execute/:other") {
+    games.synchronized {
+      val gameId = params("game").toInt
+      val game = games(gameId)
+      val playerId = params("player").toInt
+      val otherId = params("other").toInt
+      val actionsJson = request.body
+      val statsOwn = game.stats(playerId)
+      val statsOther = game.stats(playerId)
+      if (actionsJson.length() > 512) {
+        "400 action string too long (don't cheat!)"
+      } else if (!statsOwn.alive) {
+        "400 Dead players tell no lies"
+      } else if (!statsOther.alive) {
+        "400 It's dead Jim, it's dead"
+      } else {
+        val (roomOwn, roomOwnIdx) = game.map.rooms.zipWithIndex.find(_._1.players.contains(playerId)).get
+        val (roomOther, roomOtherIdx) = game.map.rooms.zipWithIndex.find(_._1.players.contains(otherId)).get
+        if (roomOwnIdx != roomOtherIdx) {
+          "400 player is not there"
+        } else {
+          val start = game.chain.find(act => act match {
+            case PlayerStartAttackAct(p, o, _) => p == playerId && o == otherId
+            case _                             => false
+          }).map(_.asInstanceOf[PlayerStartAttackAct])
+          if (!start.isDefined) {
+            "400 you never started the attack"
+          } else if (!(start.get.actionsHash.equals(GameUtils.hash(actionsJson)))) {
+            "400 hashes do not match! cheater!"
+          } else {
+            val act = PlayerFinishAttackAct(playerId, otherId, actionsJson)
+            val gameChained = game.copy(chain = act :: game.chain)
+            val otherStart = game.chain.find(act => act match {
+              case PlayerStartAttackAct(p, o, _) => p == otherId && o == playerId
+              case _                             => false
+            }).map(_.asInstanceOf[PlayerStartAttackAct])
+            val otherAttack = game.chain.find(act => act match {
+              case PlayerFinishAttackAct(p, o, _) => p == otherId && o == playerId
+              case _                              => false
+            }).map(_.asInstanceOf[PlayerFinishAttackAct])
+            val (possiblyCombatChained, msg) = if (otherStart.isDefined && otherAttack.isDefined && otherAttack.get.timestamp > otherStart.get.timestamp) {
+              // execute combat
+              val (startA, startB, outcome) = GameUtils.combat(act, otherAttack.get)
+              val combatChained = gameChained.copy(chain = CombatAct(playerId, otherId, startA, startB, actionsJson, otherAttack.get.actionsString, outcome) :: gameChained.chain)
+
+              val deadChained = if (outcome.dead.size > 0) {
+                //TODO process deaths
+                (gameChained, "200 some died?")
+              } else combatChained
+              (combatChained, "200 both live")
+            } else (gameChained, "200 awaiting other player")
+
+            games += gameId -> possiblyCombatChained
+            msg
+          }
+        }
       }
     }
   }
@@ -92,66 +183,7 @@ class GameServlet extends ScalatraServlet {
   get("/api/state/:game") {
     games.synchronized {
       val game = games(params("game").toInt)
-
-      val obj = new JSONObject()
-      var players: Map[PlayerId, JSONObject] = Map()
-      for (player <- game.players.values) {
-        val playerObj = new JSONObject()
-        val stats = game.stats(player.id)
-        playerObj.put("id", player.id)
-        playerObj.put("name", player.name)
-        playerObj.put("alive", stats.alive)
-        val killsArray = new JSONArray()
-        for (killed <- stats.kills) {
-          killsArray.put(killed)
-        }
-        playerObj.put("kills", killsArray)
-        val holdArray = new JSONArray()
-        for (holdId <- game.holding(player.id)) {
-          holdArray.put(game.objects(holdId)._type.name)
-        }
-        playerObj.put("holds", holdArray)
-
-        players += player.id -> playerObj
-      }
-
-      val rows = new JSONArray()
-      for (r <- 0.to(4)) {
-        val row = new JSONArray()
-        for (c <- 0.to(4)) {
-          val roomObj = new JSONObject()
-          roomObj.put("row", r)
-          roomObj.put("col", c)
-          val playersInRoom = new JSONArray()
-          val room = game.map.rooms.find(_.location == Tuple2(r, c)).get
-          for (player <- room.players) {
-            players(player).put("row", r)
-            players(player).put("col", c)
-            playersInRoom.put(player)
-          }
-          val objectsInRoom = new JSONArray()
-          for (obj <- room.objects) {
-            val objObj = new JSONObject()
-            objObj.put("id", obj)
-            objObj.put("type", game.objects(obj)._type.name)
-            objectsInRoom.put(objObj)
-          }
-          roomObj.put("players", playersInRoom)
-          roomObj.put("objects", objectsInRoom)
-          roomObj.put("active", room.open)
-          row.put(roomObj)
-        }
-        rows.put(row)
-      }
-
-      val playerArray = new JSONArray()
-      for (playerObj <- players.values) {
-        playerArray.put(playerObj)
-      }
-      obj.put("players", playerArray)
-      obj.put("rooms", rows)
-
-      obj.toString
+      GameUtils.toJson(game)
     }
   }
 }
